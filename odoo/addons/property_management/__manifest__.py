f1f24cf710a84e455d46e1ba389ae7eab8e56676
bartek <bart@genotic.com>
2023-04-22 21:33:40 +0200

    basic property model and view

---
+++ odoo/addons/property_management/__manifest__.py
@@ -0,0 +1,21 @@
+
+{
+    'name': 'Property Management',
+    'version': '15.0.1.0.0',
+    'summary': 'Property Management',
+    'sequence': -99,
+    'description': 'Property Management',
+    'category': 'Project',
+    'website': 'www.example.com',
+    'depends': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/property.xml'
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+
+}
---
+++ odoo/addons/property_management/models/property.py
@@ -0,0 +1,45 @@
+from odoo import models, fields, api
+
+
+class Property(models.Model):
+
+    _name = 'property.property'
+    _description = 'Property Management Property'
+
+    name = fields.Char(string='Name', required=True)
+    # image = fields.Binary(string='Image')
+    description = fields.Text(string='Description')
+    address = fields.Char(string='Address')
+    city = fields.Char(string='City')
+    state = fields.Char(string='State')
+    zip = fields.Char(string='Zip')
+    country = fields.Char(string='Country')
+    property_type = fields.Selection([
+        ('house', 'House'),
+        ('apartment', 'Apartment'),
+        ('condo', 'Condo'),
+        ('townhouse', 'Townhouse'),
+        ('other', 'Other'),
+    ], string='Property Type', default='house')
+    bedrooms = fields.Integer(string='Bedrooms')
+    bathrooms = fields.Integer(string='Bathrooms')
+    square_feet = fields.Integer(string='Square Feet')
+    rent = fields.Float(string='Rent')
+    photo_ids = fields.Many2many('property.photo', string='Photos')
+    # tenant_id = fields.Many2one('res.partner', string='Tenant')
+    # tenant_start_date = fields.Date(string='Tenant Start Date')
+    # tenant_end_date = fields.Date(string='Tenant End Date')
+    # tenant_security_deposit = fields.Float(string='Tenant Security Deposit')
+    # tenant_security_deposit_refunded = fields.Boolean(string='Tenant Security Deposit Refunded')
+    # tenant_security_deposit_refunded_date = fields.Date(string='Tenant Security Deposit Refunded Date')
+    # tenant_security_deposit_refunded_amount = fields.Float(string='Tenant Security Deposit Refunded Amount')
+    # tenant_security_deposit_refunded_notes = fields.Text(string='Tenant Security Deposit Refunded Notes')
+    # tenant_security_deposit_refunded_by = fields.Many2one('res.partner', string='Tenant Security Deposit Refunded By')
+    # tenant_security_deposit_refunded_to = fields.Many2one('res.partner', string='Tenant Security Deposit Refunded To')
+    # tenant_security_deposit_refunded_check_number = fields.Char(string='Tenant Security Deposit Refunded Check Number')
+    # tenant_security_deposit_refunded_check_date = fields.Date(string='Tenant Security Deposit Refunded Check Date')
+    # tenant_security_deposit_refunded_check_amount = fields.Float(string='Tenant Security Deposit Refunded Check Amount')
+    # tenant_security_deposit_refunded_check_notes = fields.Text(string='Tenant Security Deposit Refunded Check Notes')
+    # tenant_security_deposit_refunded_check_by = fields.Many2one('res.partner', string='Tenant Security Deposit Refunded Check By')
+    # tenant_security_deposit_refunded_check_to = fields.Many2one('res.partner', string='Tenant Security Deposit Refunded')
+
